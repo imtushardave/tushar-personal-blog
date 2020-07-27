d8d9364dc44bd9d656e808e0c121d2a3d0ad944a
Tushar Dave <imtushardave@gmail.com>
2020-07-27 14:51:23 +0530

    Google Analytics added to the website

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,5 +55,12 @@ module.exports = {
       options: {
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-150450656-1",
+      },
+    },
   ],
 };
